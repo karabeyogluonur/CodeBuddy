4fa41a0394bdbb48f83ef23d7e07f70ae92416ca
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-04-19 10:34:47 +0300

    Talent update model validator added.

---
+++ Core/CB.Application/Validations/Admin/Features/TalentUpdateModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using CB.Application.Models.Admin.Features;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CB.Application.Validations.Admin.Features
+{
+    public class TalentUpdateModelValidator : AbstractValidator<TalentUpdateModel>
+    {
+        public TalentUpdateModelValidator()
+        {
+            RuleFor(t => t.Name)
+               .NotEmpty().WithMessage("Yetenek adı boş geçilemez.")
+               .NotNull().WithMessage("Yetenek adı boş geçilemez.");
+
+        }
+    }
+}
